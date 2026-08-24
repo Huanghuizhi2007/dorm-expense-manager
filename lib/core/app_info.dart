bd6e04dca9df8479b6b0d4baba35a1609cee6f29
Huanghuizhi2007 <zz819007@outlook.com>
2026-08-24 22:00:46 +0800

    Bump version to 1.0.11

--- lib/core/app_info.dart
+++ lib/core/app_info.dart
@@ -1,7 +1,7 @@
 class AppInfo {
   AppInfo._();
 
-  static const String version = '1.0.10';
+  static const String version = '1.0.11';
   static const String releasesUrl =
       'https://github.com/Huanghuizhi2007/dorm-expense-manager/releases';
 }
